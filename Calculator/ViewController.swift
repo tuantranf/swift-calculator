558a0d4a7e24274c2845cdbb1b849b328e2a0267
Tran Minh Tuan <tuantranf@gmail.com>
2017-12-05 23:24:33 +0700

    implement calculator function

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Expression
 
 class ViewController: UIViewController {
+    @IBOutlet weak var formulaInput: UIButton!
 
+    @IBOutlet weak var fomulaLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        fomulaLabel.text = ""
+        resultLabel.text = ""
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,58 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func inputFormula(_ sender: UIButton) {
+        guard let formulaText = fomulaLabel.text else {
+            return
+        }
+        
+        guard let senderedText = sender.titleLabel?.text else {
+            return
+        }
+        
+        fomulaLabel.text = formulaText + senderedText
+    }
+    
+    @IBAction func clearCalculation(_ sender: UIButton) {
+        fomulaLabel.text = ""
+        resultLabel.text = ""
+    }
+    
+    @IBAction func calculateAnswer(_ sender: UIButton) {
+        guard let formulaText = fomulaLabel.text else {
+            return
+        }
+        let formula: String = formatFormula(formulaText)
+        resultLabel.text = evalFormula(formula)
+    }
+    
+    private func formatFormula(_ formula: String) -> String {
+        let formatted: String = formula.replacingOccurrences(
+            of: "(?<=^|[÷×\\+\\-\\(])([0-9]+)(?=[÷×\\+\\-\\)]|$)",
+            with: "$1.0",
+            options: NSString.CompareOptions.regularExpression,
+            range: nil
+            ).replacingOccurrences(of: "÷", with: "/").replacingOccurrences(of: "×", with: "*")
+        return formatted
+    }
+    
+    private func evalFormula(_ formula: String) -> String {
+        do {
+            let expression = Expression(formula)
+            let answer = try expression.evaluate()
+            return formatAnswer(String(answer))
+        } catch {
+            return "inputted formula is not correct"
+        }
+    }
+    
+    private func formatAnswer(_ answer: String) -> String {
+        let formattedAnswer: String = answer.replacingOccurrences(
+            of: "\\.0$",
+            with: "",
+            options: NSString.CompareOptions.regularExpression,
+            range: nil)
+        return formattedAnswer
+    }
 }
 
